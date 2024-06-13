fa906470d1b18ebd7707e94e9bb4147ddeeebbc1
moul <94029+moul@users.noreply.github.com>
2024-06-13 10:21:46 +0200

    chore: setup more cal metadata
    Signed-off-by: moul <94029+moul@users.noreply.github.com>

--- main.go
+++ main.go
@@ -7,8 +7,8 @@ import (
 	"os"
 	"time"
 
-	ical "github.com/arran4/golang-ical"
 	"github.com/BurntSushi/toml"
+	ical "github.com/arran4/golang-ical"
 )
 
 type Event struct {
@@ -21,8 +21,8 @@ type Config struct {
 }
 
 func main() {
-	configFile := flag.String("config", "", "Path to the config file (use '-' for stdin)")
-	outputFile := flag.String("output", "", "Path to the output file (use '-' for stdout)")
+	configFile := flag.String("config", "-", "Path to the config file (use '-' for stdin)")
+	outputFile := flag.String("output", "-", "Path to the output file (use '-' for stdout)")
 	flag.Parse()
 
 	if *configFile == "" || *outputFile == "" {
@@ -67,6 +67,12 @@ func main() {
 func generateICal(config Config, output io.Writer) error {
 	cal := ical.NewCalendar()
 	cal.SetMethod(ical.MethodPublish)
+	cal.SetName("VanityCal 💚")
+	cal.SetDescription("")
+	cal.SetTimezoneId("Europe/Paris")
+	cal.SetTzid("Europe/Paris")
+	cal.SetCalscale("GREGORIAN")
+	cal.SetLastModified(time.Now()) // XXX: take last modification date of this binary AND the input.
 
 	for _, event := range config.Events {
 		date, err := time.Parse("2006-01-02", event.Date)
@@ -77,8 +83,10 @@ func generateICal(config Config, output io.Writer) error {
 		anniversaries := getAnniversaries(date)
 		for _, anniv := range anniversaries {
 			duration := getDuration(date, anniv)
-			icalEvent := cal.AddEvent(fmt.Sprintf("anniv-%s", anniv.Format("20060102")))
-			icalEvent.SetSummary(fmt.Sprintf("%s - %s", event.Title, duration))
+			uuid := fmt.Sprintf("vanitycal-%s", anniv.Format("20060102"))
+			icalEvent := cal.AddEvent(uuid)
+			summary := fmt.Sprintf("%s - %s 💚", event.Title, duration)
+			icalEvent.SetSummary(summary)
 
 			// fullday
 			icalEvent.SetProperty(ical.ComponentPropertyDtStart, anniv.UTC().Format("20060102"), ical.WithValue("DATE"))
@@ -95,32 +103,32 @@ func generateICal(config Config, output io.Writer) error {
 
 func getAnniversaries(date time.Time) []time.Time {
 	return []time.Time{
-		date.AddDate(1, 0, 0),    // 1 year
-		date.AddDate(2, 0, 0),    // 2 years
-		date.AddDate(3, 0, 0),    // 3 years
-		date.AddDate(4, 0, 0),    // 4 years
-		date.AddDate(5, 0, 0),    // 5 years
-		date.AddDate(6, 0, 0),    // 6 years
-		date.AddDate(7, 0, 0),    // 7 years
-		date.AddDate(8, 0, 0),    // 8 years
-		date.AddDate(9, 0, 0),    // 9 years
-		date.AddDate(10, 0, 0),    // 10 years
-		date.AddDate(15, 0, 0),    // 15 years
-		date.AddDate(20, 0, 0),    // 20 years
-		date.AddDate(25, 0, 0),    // 25 years
-		date.AddDate(30, 0, 0),    // 30 years
-		date.AddDate(35, 0, 0),    // 35 years
-		date.AddDate(40, 0, 0),    // 40 years
-		date.AddDate(45, 0, 0),    // 45 years
-		date.AddDate(50, 0, 0),    // 50 years
-		date.AddDate(0, 0, 100),   // 100 days
-		date.AddDate(0, 0, 1_000), // 1 000 days
-		date.AddDate(0, 0, 10_000), // 10 000 days
+		date.AddDate(1, 0, 0),       // 1 year
+		date.AddDate(2, 0, 0),       // 2 years
+		date.AddDate(3, 0, 0),       // 3 years
+		date.AddDate(4, 0, 0),       // 4 years
+		date.AddDate(5, 0, 0),       // 5 years
+		date.AddDate(6, 0, 0),       // 6 years
+		date.AddDate(7, 0, 0),       // 7 years
+		date.AddDate(8, 0, 0),       // 8 years
+		date.AddDate(9, 0, 0),       // 9 years
+		date.AddDate(10, 0, 0),      // 10 years
+		date.AddDate(15, 0, 0),      // 15 years
+		date.AddDate(20, 0, 0),      // 20 years
+		date.AddDate(25, 0, 0),      // 25 years
+		date.AddDate(30, 0, 0),      // 30 years
+		date.AddDate(35, 0, 0),      // 35 years
+		date.AddDate(40, 0, 0),      // 40 years
+		date.AddDate(45, 0, 0),      // 45 years
+		date.AddDate(50, 0, 0),      // 50 years
+		date.AddDate(0, 0, 100),     // 100 days
+		date.AddDate(0, 0, 1_000),   // 1 000 days
+		date.AddDate(0, 0, 10_000),  // 10 000 days
 		date.AddDate(0, 0, 100_000), // 100 000 days
-		date.AddDate(0, 1, 0),    // 1 month
-		date.AddDate(0, 2, 0),    // 2 month
-		date.AddDate(0, 3, 0),    // 3 month
-		date.AddDate(0, 6, 0),    // 6 months
+		date.AddDate(0, 1, 0),       // 1 month
+		date.AddDate(0, 2, 0),       // 2 month
+		date.AddDate(0, 3, 0),       // 3 month
+		date.AddDate(0, 6, 0),       // 6 months
 	}
 }
 
